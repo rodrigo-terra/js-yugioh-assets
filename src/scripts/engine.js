e14e24e051e70ef64bbd6d17eee1c27a8ca9283c
Rodrigo Terra <rodrigo93terra@gmail.com>
2024-12-12 20:15:58 -0300

    Criando a enumeração de cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,23 +1,53 @@
 
 const state = {
-  score:{
+  score: {
     playerScore: 0,
     computerScore: 0,
     scoreBox: document.getElementById("score_points"),
   },
-  cardSprite:{
+  cardSprite: {
     avatar: document.getElementById("card-image"),
     name: document.getElementById("card-name"),
     type: document.getElementById("card-type"),
   },
-  fieldCards:{
+  fieldCards: {
     player: document.getElementById("player-field-card"),
     computer: document.getElementById("computer-field-card"),
   },
-  actions:{
+  actions: {
     button: document.getElementById("next-duel"),
   },
 }
-function init() {}
+
+const pathImages = "src/assets/icons/";
+
+const cardData = [
+  {
+    id: 0,
+    name: "Blue Eyes White Dragon",
+    type: "Papel",
+    img: `${pathImages}dragon.png`,
+    WinOf: [1],
+    LoseOf: [2],
+  },
+  {
+    id: 1,
+    name: "Dark Magician",
+    type: "Rock",
+    img: `${pathImages}magician.png`,
+    WinOf: [2],
+    LoseOf: [0],
+  },
+  {
+    id: 2,
+    name: "Exodia",
+    type: "Scissors",
+    img: `${pathImages}exodia.png`,
+    WinOf: [0],
+    LoseOf: [1],
+  },
+]
+
+function init() { }
 
 init()
