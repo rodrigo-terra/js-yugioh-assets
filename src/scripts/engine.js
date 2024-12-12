2ccfc3f2d0de9daff677d953003751f82dd1ee25
Rodrigo Terra <rodrigo93terra@gmail.com>
2024-12-12 20:42:38 -0300

    Criando assinatura das funções principais

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -19,6 +19,11 @@ const state = {
   },
 }
 
+const playerSides = {
+  player1: "player-field-card",
+  computer: "computer-field-card",
+};
+
 const pathImages = "src/assets/icons/";
 
 const cardData = [
@@ -48,6 +53,18 @@ const cardData = [
   },
 ]
 
-function init() { }
+async function drawCards(carNumbers, fieldSide) {
+  for (let i = 0; i < carNumbers; i++) {
+    const randomIdCard = await getRandomCardId();
+    const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+    document.getElementById(fieldSide).appendChild();
+  }
+}
+
+function init() {
+  drawCards(5, playerSides.player1);
+  drawCards(5, playerSides.computer);
+}
 
 init()
