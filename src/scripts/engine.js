5af9a0a174900791be3a8cba2a858905aa56042a
Rodrigo Terra <rodrigo93terra@gmail.com>
2024-12-12 22:04:37 -0300

    Implementando a lógica de setar cartas em campo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -53,12 +53,37 @@ const cardData = [
   },
 ]
 
+async function getRandomCardId() {
+  const randomIndex = Math.floor(Math.random() * cardData.length);
+  return cardData[randomIndex].id;
+}
+
+async function createCardImage(IdCard, fieldSide) {
+  const cardImage = document.createElement("img");
+  cardImage.setAttribute("height", "100px");
+  cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
+  cardImage.setAttribute("data-id", IdCard);
+  cardImage.classList.add("card");
+
+  if (fieldSide === playerSides.player1) {
+    cardImage.addEventListener("click", () => {
+      setCardsField(cardImage.getAttribute("data-id"));
+    });
+  }
+
+  cardImage.addEventListener("mouseover", () => {
+    drawSelectCard(IdCard);
+  })
+
+  return cardImage;
+}
+
 async function drawCards(carNumbers, fieldSide) {
   for (let i = 0; i < carNumbers; i++) {
     const randomIdCard = await getRandomCardId();
     const cardImage = await createCardImage(randomIdCard, fieldSide);
 
-    document.getElementById(fieldSide).appendChild();
+    document.getElementById(fieldSide).appendChild(cardImage);
   }
 }
 
