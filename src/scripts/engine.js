00ff10df474ba073b2af84cf9ffe1418b5c02c87
Rodrigo Terra <rodrigo93terra@gmail.com>
2024-12-12 22:35:45 -0300

    Implementando o setCardFields

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -85,6 +85,23 @@ async function createCardImage(IdCard, fieldSide) {
   return cardImage;
 }
 
+async function setCardsField(cardId) {
+  await removeAllCardsImages();
+
+  let computerCardId = await getRandomCardId();
+
+  state.fieldCards.player.style.display = "block";
+  state.fieldCards.computer.style.display = "block";
+
+  state.fieldCards.player.src = cardData[cardId].img;
+  state.fieldCards.computer.src = cardData[computerCardId].img;
+
+  let duelResults = await checkDuelResults(cardId, computerCardId);
+
+  await updateScore();
+  await drawButton(duelResults);
+}
+
 async function drawCards(carNumbers, fieldSide) {
   for (let i = 0; i < carNumbers; i++) {
     const randomIdCard = await getRandomCardId();
